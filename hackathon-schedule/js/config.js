bff890faea1fae08ecd6365e9245ab9aa44cd2f1
Omar Firdaus <omar.firdaus101@gmail.com>
2026-02-26 21:51:03 -0600

    Update API keys in config

--- hackathon-schedule/js/config.js
+++ hackathon-schedule/js/config.js
@@ -5,12 +5,12 @@
 
 // The ID is the long string in the sheet URL: 
 // https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/edit
-export const SHEET_ID = 'YOUR_SHEET_ID_HERE'; 
+export const SHEET_ID = '1dS0CVnKdzaCgZmGTgnuKRNSCNPZAY0m2SMnGXjc8hms';
 
 // You can generate an API Key from the Google Cloud Console.
 // Make sure to restrict it to the Google Sheets API.
-export const API_KEY = 'YOUR_API_KEY_HERE';
+export const API_KEY = 'AIzaSyCCBa31VpshQdowh15w0lNNVh9x34Ud92o';
 
-// The range to fetch from the sheet (e.g., 'Sheet1!A:J' or simply 'Sheet1' to get everything).
+// The range to fetch from the sheet `=(e.g., 'Sheet1!A:J' or simply 'Sheet1' to get everything).
 // Update this if your sheet name is different or you only want a specific range.
 export const SHEET_RANGE = 'Sheet1';
